2ff26e34e9e299a989ad2016890eaf347453090b
oiyr <94066885+oiyr@users.noreply.github.com>
2021-11-10 17:54:17 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,6 +1,7 @@
---is a load script
-
 if game.PlaceId == 155615604 then
   print('loaded');
   loadstring(game:HttpGet('https://oiyr.github.io/g/prison_life.lua'))();
+elseif game.PlaceId == 5535087806 then
+  print('loaded');
+  loadstring(game:HttpGet('https://oiyr.github.io/g/tapping_mania.lua'))();
 end;
